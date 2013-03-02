1cdc3b77ca9497c8c7a75447fb728c047c81f6b8
008agent <008agent@mail.ru>
2013-03-02 16:40:38 +0400

    first lab commit

---
+++ lab1/HelloWorld/src/helloworld/Extender.java
@@ -0,0 +1,40 @@
+package helloworld;
+
+class Extender extends HelloWorld implements Iprintable
+{
+    public Extender( String _ms )
+    {
+        this.msg = _ms;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return this.msg;
+    }
+
+    @Override
+    public void print() 
+    {
+        System.out.println( this.toString() );
+    }
+    
+    @Override 
+    public void flush()
+    {
+        this.msg = "";
+    }
+
+    @Override
+    public String get_field() 
+    {
+        return this.msg;
+    }
+
+    @Override
+    public void set_field(String arg) 
+    {
+        this.msg = arg;
+    }
+    
+}
---
+++ lab1/HelloWorld/src/helloworld/Iprintable.java
@@ -0,0 +1,15 @@
+
+package helloworld;
+
+public interface Iprintable 
+{
+    public abstract void print();
+    
+    public abstract void flush();
+    
+    public abstract String get_field();
+    
+    public abstract void set_field(String arg);
+    
+
+}
