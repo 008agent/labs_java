da4ac1c14c351cfa206fa82adbe8b9c94d693888
008agent <008agent@mail.ru>
2013-04-15 12:15:43 +0400

    some fixes

---
+++ lab1/Iprintable.java
@@ -0,0 +1,12 @@
+public interface Iprintable 
+{
+    public abstract void print();
+    
+    public abstract void flush();
+    
+    public abstract String get_field();
+    
+    public abstract void set_field(String arg);
+    
+
+}
